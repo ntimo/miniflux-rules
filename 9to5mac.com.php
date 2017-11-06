28557e84dd35366e243e2295b43812f7d8b49dc6
TimoViBritannia <ntimo@users.noreply.github.com>
2017-11-06 09:25:00 +0100

    + created rule for 9to5mac.com

---
+++ 9to5mac.com.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'grabber' => array(
+        '%.*%' => array(
+            'test_url' => 'https://9to5mac.com/2017/11/05/iphone-x-samsung-ad-notch-more/',
+            'body' => array(
+                '//div[@class="post-body"]',
+            ),
+        ),
+    ),
+);
