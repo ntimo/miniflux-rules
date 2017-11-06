f7013438dc03b7ac3b38a74e9342c16fed9ca44e
TimoViBritannia <ntimo@users.noreply.github.com>
2017-11-06 13:10:15 +0100

    + created rule for maclife.de

---
+++ maclife.de.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'grabber' => array(
+        '%.*%' => array(
+            'test_url' => 'http://www.maclife.de/ratgeber/iphone-x-verwechseln-zwischen-apps-10097442.html',
+            'body' => array(
+                '//div[@class="article_wrapper intellitxt"]',
+            ),
+        ),
+    ),
+);
